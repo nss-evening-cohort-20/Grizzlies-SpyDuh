271f2a8dafd9966046a212250a3086bb20911678
Mariana Mena <maranashokry@ahoo.com>
2023-03-15 09:45:11 -0500

    Insert User And Agency database

---
+++ Grizzlies-SpyDuh/Database/Agency.sql
@@ -0,0 +1,13 @@
+USE [SpyDuh]
+GO
+INSERT INTO [dbo].[Agency]
+           ([Handler]
+           ,[Name])
+     VALUES(0,'Michael Westen'),
+           (1,'John Drake'),
+           (0,'Edgar Brodie'),
+           (0,'Richard Hannay'),
+           (1,'Alicia Huberman')    
+GO
+
+
