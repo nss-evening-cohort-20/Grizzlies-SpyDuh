e01f2b38537851b965728c7bebbc9aa1f9e27b6e
Mariana Mena <maranashokry@ahoo.com>
2023-03-15 10:31:12 -0500

    Added User and Agency Models

---
+++ Grizzlies-SpyDuh/Database/User.sql
@@ -0,0 +1,32 @@
+USE [SpyDuh]
+GO
+INSERT INTO [dbo].[User]
+           ([Name]
+           ,[Email]
+           ,[AgencyId])
+     VALUES
+           ('Harry Palmer', 'hpalmer@Spyduh.org',2),
+           ('Tom Bishop', 'tpishop@Spyduh.org',1),
+           ('Roger Thornhill', 'rhornhill@Spyduh.org',5),
+           ('Annie Walker', 'aWalker@Spyduh.org',3),
+           ('Black Widow', 'bwidow@Spyduh.org',5),
+           ('William Brandt', 'wbrandt@Spyduh.org',2),
+           ('Felix Leiter', 'fleiterr@Spyduh.org',4),
+           ('Nathan Muir', 'nmuir@Spyduh.org',1),
+           ('Evelyn Salt', null,3),
+           ('Carrie Mathison', null,1),
+           ('Jack Ryan', 'jryan@Spyduh.org',4),
+           ('Nick Fury', 'nfury@Spyduh.org',2),
+           ('Sarah Walker', 'swalker@Spyduh.org',5),
+           ('Austin Powers', 'apowers@Spyduh.org',3),
+           ('Emma Peel', null,4),
+           ('Napoleon Solo', 'nsolo.org',3),
+           ('Harry Hart', 'hart@Spyduh.org',2),
+           ('Maxwell Smart', null,4),
+           ('Sydney Bristow', 'sbristow@Spyduh.org',5),
+           ('George Smiley', 'gsmiley@Spyduh.org',3),
+           ('Ethan Hunt', null,4),
+           ('Illya Kuryakin', null,4)
+GO
+
+
