2e44588acc1a3c8da1c5e56ac76ae350c5c30a73
alexcueto88 <wonderwomanoftech@gmail.com>
2023-03-15 19:39:29 -0500

    Added UserSkill model

---
+++ Grizzlies-SpyDuh/Models/UserSkill.cs
@@ -0,0 +1,13 @@
+﻿namespace Grizzlies_SpyDuh.Models
+{
+    public class UserSkill
+    {
+        public int Id { get; set; } 
+
+        public int UserId { get; set; }
+
+        public int SkillId { get; set; }
+
+        public int SkillLevel { get; set;}
+    }
+}
