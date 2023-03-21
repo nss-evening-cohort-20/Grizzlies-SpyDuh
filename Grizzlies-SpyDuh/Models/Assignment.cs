875c331537cd9f01713aeadc1d614a6d901d378c
Jeremy White <jcwhite414@gmail.com>
2023-03-20 21:40:01 -0500

    Added Assignment and User Assignment models

---
+++ Grizzlies-SpyDuh/Models/Assignment.cs
@@ -0,0 +1,16 @@
+﻿namespace Grizzlies_SpyDuh.Models;
+
+public class Assignment
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public int AgencyId { get; set; }
+    public Agency Agency { get; set; }
+    public bool Fatal { get; set; }
+    public DateTime StartMissionDateTime { get; set; }
+    public DateTime EndMissionDateTime { get; set;}
+
+    public List<UserAssignment> UserAssignments { get; set; }
+
+    public int DaysRemainingInAssignment => (EndMissionDateTime - DateTime.Today).Days;
+}
