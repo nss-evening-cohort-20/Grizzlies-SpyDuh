b3e05fe0f50da279996918560e9a8b81f22f9419
Jeremy White <jcwhite414@gmail.com>
2023-03-21 07:27:55 -0500

    Assignment controller wired up

---
+++ Grizzlies-SpyDuh/Controllers/AssignmentController.cs
@@ -0,0 +1,24 @@
+﻿using Grizzlies_SpyDuh.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grizzlies_SpyDuh.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AssignmentController : ControllerBase
+    {
+        private readonly IAssignmentRepository _assignmentRepository;
+
+        public AssignmentController(IAssignmentRepository assignmentRepository)
+        {
+            _assignmentRepository = assignmentRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_assignmentRepository.GetAll());
+        }
+    }
+}
--- Grizzlies-SpyDuh/Program.cs
+++ Grizzlies-SpyDuh/Program.cs
@@ -1,4 +1,5 @@
 using Grizzlies_SpyDuh.Repositories;
+using Grizzlies_SpyDuh.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IAssignmentRepository, AssignmentRepository>();
 
 var app = builder.Build();
 
